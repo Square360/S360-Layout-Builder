ca31c22a69e0f231d9b5634f8bbf7ec7241b4874
Steven Keiser <steven.keiser@square360.com>
2025-10-17 19:13:44 -0400

    feat(): add custom field formatter for accordion summary

---
+++ src/Plugin/Field/FieldFormatter/S360LayoutBuilderAccordionSummaryFormatter.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Drupal\s360_layout_builder\Plugin\Field\FieldFormatter;
+
+use Drupal\advanced_header_field\AdvancedHeaderFieldInterface;
+use Drupal\Core\Field\FieldItemInterface;
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\FormatterBase;
+
+/**
+ * Plugin implementation of the 's360_layout_builder' summary formatter.
+ *
+ * @FieldFormatter(
+ *   id = "s360_layout_builder_accordion_summary",
+ *   label = @Translation("Accordion Summary Markup"),
+ *   field_types = {
+ *     "advanced_header_field"
+ *   }
+ * )
+ */
+class S360LayoutBuilderAccordionSummaryFormatter extends FormatterBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    $elements = [];
+
+    foreach ($items as $delta => $item) {
+      $elements[$delta] = $this->viewElement($item);
+    }
+
+    return $elements;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function viewElement(FieldItemInterface $item) {
+    $values = $item->getValue();
+
+    if (!$values['title']) {
+      return;
+    }
+
+    $options = $values['options'];
+
+    $parent_id = AdvancedHeaderFieldInterface::getHeaderParentId($item);
+
+    $element = [
+      '#type' => 'html_tag',
+      '#tag' => 'summary',
+      '#value' => $values['title'],
+      '#attributes' => [
+        'id' => !empty($options['custom_anchor_id'])
+          ? $options['custom_anchor_id']
+          : AdvancedHeaderFieldInterface::createAnchorIdFromText($values['title'], $parent_id),
+      ],
+    ];
+
+    return $element;
+  }
+
+}
